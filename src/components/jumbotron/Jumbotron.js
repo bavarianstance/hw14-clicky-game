e69a6664ef252b439c571f6dc6e61295b22cf095
bavarianstance <bavarianstance@gmail.com>
2019-04-24 16:55:03 -0700

    add jumbotron component

---
+++ src/components/jumbotron/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => {
+	return (<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">React.js Memory Game</h1>
+    <p className="lead">Click on any logo to increase score, but if you hit a dupe - game over!</p>
+  </div>
+</div>
+
+	);
+}
+
+export default Jumbotron; 
