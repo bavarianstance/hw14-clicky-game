2ee419aedd4775ebdc53ef12fb0f661cf9781e1b
bavarianstance <bavarianstance@gmail.com>
2019-04-25 10:09:02 -0700

    Updates

---
+++ precache-manifest.c60339bd820c1e2a4307ca1a877759df.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2427098253d14f4699c10620c1f0e00d",
+    "url": "/hw14-clicky-game/index.html"
+  },
+  {
+    "revision": "51365b7d598eaf294290",
+    "url": "/hw14-clicky-game/static/css/main.96eb258c.chunk.css"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/hw14-clicky-game/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "51365b7d598eaf294290",
+    "url": "/hw14-clicky-game/static/js/main.b3b95489.chunk.js"
+  },
+  {
+    "revision": "9395debace662bb7d881",
+    "url": "/hw14-clicky-game/static/js/runtime~main.019757e5.js"
+  },
+  {
+    "revision": "747d3a21c0b1e5639b2de81fe7cc530e",
+    "url": "/hw14-clicky-game/static/media/audi.747d3a21.png"
+  },
+  {
+    "revision": "b9058a467958b0736fa20060edc906e1",
+    "url": "/hw14-clicky-game/static/media/bmw.b9058a46.jpg"
+  },
+  {
+    "revision": "d495cff545442e89673291b648211c06",
+    "url": "/hw14-clicky-game/static/media/ferrari.d495cff5.jpg"
+  },
+  {
+    "revision": "287a68889195a4577be88f4146de69bf",
+    "url": "/hw14-clicky-game/static/media/ford.287a6888.jpg"
+  },
+  {
+    "revision": "8f6c0f8355d014a10701253199eeda0c",
+    "url": "/hw14-clicky-game/static/media/hyundai.8f6c0f83.png"
+  },
+  {
+    "revision": "7d2f41ddd7d80570b0a2172135c1c925",
+    "url": "/hw14-clicky-game/static/media/lambo.7d2f41dd.jpg"
+  },
+  {
+    "revision": "d1f3da1f5c833232eeb93c0384410135",
+    "url": "/hw14-clicky-game/static/media/m5.d1f3da1f.jpg"
+  },
+  {
+    "revision": "e9e103b8e05898e20f443f010022a084",
+    "url": "/hw14-clicky-game/static/media/mazda.e9e103b8.jpg"
+  },
+  {
+    "revision": "ef71d13aa6a3325ea99e190ecfae2991",
+    "url": "/hw14-clicky-game/static/media/nissan.ef71d13a.png"
+  },
+  {
+    "revision": "03cf7501b423327d028db849f59246a2",
+    "url": "/hw14-clicky-game/static/media/porsche.03cf7501.jpg"
+  },
+  {
+    "revision": "9e96cd3283953e989a30425724523271",
+    "url": "/hw14-clicky-game/static/media/subaru.9e96cd32.png"
+  },
+  {
+    "revision": "2bd2a6596e790a94d20aae4141320b85",
+    "url": "/hw14-clicky-game/static/media/toyota.2bd2a659.jpg"
+  }
+]);
