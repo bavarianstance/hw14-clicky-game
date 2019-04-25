c4bf8095ec5dd2971039ce5ef12fc61ff8d5121b
bavarianstance <bavarianstance@gmail.com>
2019-04-24 17:06:54 -0700

    added mfg container

---
+++ src/components/mfg/mfg.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+let mfg = props => {
+	return (
+		<div className = "card" onClick = {event => props.clickEvent(event.target.src)}>
+		  <img className = "card-img-top" src = {props.name} />
+		</div>
+		);
+	}
+
+export default MFG;
